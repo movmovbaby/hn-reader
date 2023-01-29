7f9668991a0523e9b43b31b8faea80dbea168bb7
Nikolai Ochirov <dubdubman@ya.ru>
2023-01-29 10:04:08 +0300

    added auto update stories

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import { selectors as storiesSelectors } from '../../store/storiesSlice.js';
 import { actions as storiesActions } from '../../store/storiesSlice.js';
 import { getStories } from '../../api/hn-api.js';
 import NewsList from '../NewsList/NewsList.jsx';
+import styles from './Home.module.css';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -11,19 +12,28 @@ const Home = () => {
   stories.sort((a, b) => a.time > b.time ? -1 : 1);
 
   const refreshStories = async () => {
-    console.log('refresh stories')
+
     const stories = await getStories();
     dispatch(storiesActions.addStories(stories));
-
-    return setTimeout(() => refreshStories(), 5000);
   };
 
   useEffect(() => {
-    refreshStories();
+    const timer = setTimeout(() => {
+      console.log('refresh stories');
+      refreshStories();
+    }, 60000);
+
+    return () => clearInterval(timer);
   }, [])
 
   return (
-    <NewsList stories={stories} />
+    <>
+      <button className={styles['refresh-button']} onClick={() => {
+        console.log('button refresh stories');
+        refreshStories();
+      }}>Refresh stories</button>
+      <NewsList stories={stories} />
+    </>
   )
 };
 
