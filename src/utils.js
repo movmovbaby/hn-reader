e5e884c5bdc16a1108a3f30086dec1ddcdbb496d
Nikolai Ochirov <dubdubman@ya.ru>
2023-02-21 23:33:11 +0300

    change date format

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 const dateFormat = (time) => {
-  const hh = new Date(time * 1000).toLocaleTimeString('ru-RU');
+  const hh = new Date(time * 1000).toLocaleTimeString('ru-RU').slice(0, -3);
   const dd = new Date(time * 1000).toLocaleDateString('ru-RU');
   return `${hh} ${dd}`;
 };
